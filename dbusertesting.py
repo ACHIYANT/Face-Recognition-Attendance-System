1aee5cbd846c6a5281012e95d5c32ee6148dd55c
ACHIYANT <55272025+ACHIYANT@users.noreply.github.com>
2022-01-05 09:37:06 +0530

    Update dbusertesting.py

--- dbusertesting.py
+++ dbusertesting.py
@@ -1,3 +1,4 @@
+#This script is used to test your database,please change your port and host as needed and also make necessary changes.
 import mysql.connector
 from mysql.connector import errorcode
 try:
@@ -18,10 +19,7 @@ except mysql.connector.Error as e:
 cursor=connectt.cursor()
 
 selectquery="select * from students"
-# insertquery="insert into students values(%s,%s,%s,%s)"
-# val=(182987,"eyuq","bchdjb","yedboi@jcnwi.com")
-# cursor.execute(insertquery,val)
-# connectt.commit()
+
 cursor.execute(selectquery)
 records=cursor.fetchall()
 print("No of user",cursor.rowcount)
@@ -34,60 +32,3 @@ for row in records:
     print()
 cursor.close()
 connectt.close
-#
-# sql = "INSERT INTO customers (name, address) VALUES (%s, %s)"
-# val = ("John", "Highway 21")
-# mycursor.execute(sql, val)
-#
-# mydb.commit()
-
-
-
-
-
-#8888888888888888888888888888888888888
-
-
-
-
-
-
-# import mysql.connector
-# from mysql.connector import (connection)
-#
-# config = {
-#   'user': 'root',
-#   'password': '',
-#   'host': '127.0.0.1',
-#   'database': 'facerecognition',
-#   'raise_on_warnings': True
-# }
-#
-# cnx = mysql.connector.connect(**config)
-#
-# cursor=cnx.cursor()
-# selectquery="select * from students"
-# cursor.execute(selectquery)
-# records=cursor.fetchall()
-# print("No of user",cursor.rowcount)
-#
-# for row in records:
-#     print("URoll",row[0])
-#     print("Name",row[1])
-#     print("Password",row[2])
-#     print("Email",row[3])
-#     print()
-# cnx.close()
-
-
-
-
-
-
-#888888888888888888888888888888888
-
-
-
-
-
-
